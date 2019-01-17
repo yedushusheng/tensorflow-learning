c6f9878c25e074c84b679c4de9c653332fa76118
yedushusheng <yedushusheng@163.com>
2019-01-18 00:18:01 +0800

    run success

--- Part1 Introduction/introduction.py
+++ Part1 Introduction/introduction.py
@@ -24,9 +24,7 @@ sess = tf.Session()
 sess.run(init)
 
 # 拟合平面
-for step in xrange(0, 201):
-    sess.run(train)
-    if step % 20 == 0:
-        print (step, sess.run(W), sess.run(b))
-
-# 得到最佳拟合结果 W: [[0.100  0.200]], b: [0.300]
+#for step in xrange(0, 201):
+#    sess.run(train)
+#    if step % 20 == 0:
+#        print  step, sess.run(W), sess.run(b)
